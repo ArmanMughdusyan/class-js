b585218be49e03f1aabd730ae9d5fe487e37b846
ArmanMughdusyan <arman.mughdusyan33@gmail.com>
2022-07-31 08:51:23 -0700

    data printing

--- class.js
+++ class.js
@@ -12,22 +12,37 @@ class Person {
         console.log(`Hi, I am ${this.name} ${this.lastName},i am ${this.gender}, i am ${this.age} years old,
          my spike is ${this.height} centimeters, my hair color is ${this.hairColor}`);
     }
-    a() {
+    genderBM() {
         if(this.gender == "male") {
             return "BOY";
         } else {
             return "GIRL";
         }
     }
+    dataPrinting() {
+        console.log(`Name - ${this.name}
+Last Name - ${this.lastName}
+Age - ${this.age}
+Height - ${this.height}
+Gender - ${this.gender}
+Hair Color - ${this.hairColor}`);
+    }
 }
 
 let person1 = new Person("Poxos", "Poxosyan", 33, 168, "male", "black");
 person1.aboutMe();
-console.log(person1.a());
+console.log(person1.genderBM());
+person1.dataPrinting();
+
 let person2 = new Person("Petros", "Petrosyan", 26, 189, "male", "yellow");
 person2.aboutMe();
-console.log(person2.a());
+console.log(person2.genderBM());
+person2.dataPrinting();
+
 let person3 = new Person("Anna", "Ananyan", 18, 156, "female", "brown");
 person3.aboutMe();
-console.log(person3.a());
+console.log(person3.genderBM());
+person3.dataPrinting();
+
+
 
