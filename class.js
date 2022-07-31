9cf1909cbd9b7582a2eb952a829b5ce6484e4b6d
ArmanMughdusyan <arman.mughdusyan33@gmail.com>
2022-07-31 10:27:46 -0700

    adult

--- class.js
+++ class.js
@@ -44,5 +44,30 @@ person3.aboutMe();
 console.log(person3.genderBM());
 person3.dataPrinting();
 
+let person4 = new Person("Gnchu", "Hovakimyan", 15, 190, "male", "black");
+let person5 = new Person("Lil", "Lalayan", 13, 120, "female", "yellow");
 
+let personArr = [];
+personArr.push(person1, person2, person3, person4, person5);
+
+for (let i = 0; i < personArr.length; i++) {
+    alert(personArr[i].age);
+};
+
+function Delete() {
+    for (let i = 0; i < personArr.length; i++) {
+        if(personArr[i].age < 18) {
+            personArr.splice(i, 1)
+            i--;
+        };
+        
+    };
+};
+
+Delete();
+
+for (let i = 0; i < personArr.length; i++) {
+    alert(personArr[i].age);
+    
+};
 
